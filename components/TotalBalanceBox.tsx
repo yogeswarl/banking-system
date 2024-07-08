9c23616bc2001fee8b1f827d73da01d3af33848f
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2024-07-07 23:12:13 -0400

    Home page, Doughnut chart with chartjs

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,12 +1,13 @@
 import { formatAmount } from '@/lib/utils'
 import React from 'react'
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({accounts,totalBanks,totalCurrentBalance}:TotalBalanceBoxProps) => {
   return (
     <section className="total-balance">
       <div className="total-balance-chart">
-      {/* DonutChart */}
+      <DoughnutChart accounts={accounts} />
       </div>
       <div className="flex flex-col gap-6">
         <h2 className="header-2">
